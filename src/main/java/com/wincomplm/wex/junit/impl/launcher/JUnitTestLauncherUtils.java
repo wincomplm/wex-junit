e31f4f7f94c51fe7ac89a32f914eb937faee6ecc
simonh-wincomplm <simonh@wincom-plm.com>
2023-02-08 22:00:01 +0100

    Coverted to wexinvoke

--- src/main/java/com/wincomplm/wex/junit/impl/launcher/JUnitTestLauncherUtils.java
+++ src/main/java/com/wincomplm/wex/junit/impl/launcher/JUnitTestLauncherUtils.java
@@ -9,6 +9,8 @@ package com.wincomplm.wex.junit.impl.launcher;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wincomplm.wex.kernel.impl.annotations.WexComponent;
+import com.wincomplm.wex.kernel.impl.annotations.WexMethod;
 import com.wincomplm.wex.security.commons.impl.WexAdminCheckAccess;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -24,15 +26,17 @@ import wt.util.WTException;
  *
  * @author SimonHeath
  */
-abstract public class JUnitTestLauncherAbstract {
-    
-    protected void runTest(HttpServletRequest httprequest, HttpServletResponse httpresponse,Class testClass) throws JsonProcessingException, WTException, IOException {
+@WexComponent(uid = "junit-methods", description = "Junit Methods")
+class JUnitTestLauncherUtils {
+        
+    @WexMethod(name = "runTest", description = "Run junit tests")
+    public void runTest(HttpServletRequest httprequest, HttpServletResponse httpresponse,Class testClass) throws Exception {
         WexAdminCheckAccess.instance.checkAccess();
         String json = runTest(testClass);
         returnTest(httprequest, httpresponse, json);
     }
     
-    protected void returnTest(HttpServletRequest httprequest, HttpServletResponse httpresponse, String json) throws IOException {
+    public void returnTest(HttpServletRequest httprequest, HttpServletResponse httpresponse, String json) throws IOException {
         boolean pretty = httprequest.getParameter("pretty")!=null;
         if (pretty) httpresponse.getOutputStream().print( "<pre>");
         httpresponse.getOutputStream().print( json);
